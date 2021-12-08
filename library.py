ead463697a8f75dc3e900c4b8c008479e1551965
FPolin22 <federico.polin@gmail.com>
2021-12-08 10:55:01 +0100

    Added number of pages and some notes, the result need to be more clear

--- library.py
+++ library.py
@@ -1,106 +1,149 @@
-list_of_books = ('Romeo and Juliet': 'William Shakespeare',
-'1984': 'George Orwell',
-'2001: A Space Odissey': 'Arthur C. Clarke',
-'Pride And Prejudice' : 'Jane Austen',
-'The Great Gatsby' : 'F.Scott Fitzgerald',
-'The Lord Of The Rings' : 'J. R. R. Tolkien',
-'The Old Man And The Sea' : 'Ernest Hemingway',
-'The Picture Of Dorian Gray' : 'Oscar Wilde',
-'A Christmas Carol' : 'Charles Dickens',
-'I Promessi Sposi' : 'Alessandro Manzoni',
-'Awake The Giant Within' : 'Tony Robbins',
-'La Divina Commedia' : 'Dante Allighieri',
-'Odissey' : 'Omero',
-'Lo Scudo Di Talos' : 'Valerio Massimo Manfredi',
-'Delitto E Castigo' : 'Dostoevskij',
-'I Giorni Dell Eternita' : 'Ken Follet',
-'It' : 'Stephen King',
-'Fu Sera Fu Mattina' : 'Ken Follet',
-'La Caduta Dei Giganti' : 'Ken Follet',
-'La Cruna Dell Ago' : 'Ken Follet',
-'Mondo Senza Fine' : 'Ken Follet',
-'L Arte Della Guerra' : 'Sun Tzu',
-'Alla Ricerca Del Tempo Perduto' : 'Marcel Proust',
-'La Coscienza Di Zeno' : 'Italo Svevo',
-'Il Processo' : 'Franz Kafka',
-'Uno, Nessuno, Centomila' : 'Luigi Pirandello',
-'Gita Al Faro' : 'Virginia Wolf',
-'Il Processo' : 'Franz Kafka',
-'Matilda' : 'Roald Dahl',
-'La Fabbrica Di Cioccolato' : 'Roald Dahl',
-'Billy Elliot' : 'Melvin Burgess',
-'Il Nome Della Rosa' : 'Umberto Eco',
-'Intelligenza Emotiva' : 'Daniel Goleman',
-'Cosa vuoi davvero?' : 'Roberto Re',
-'Intelligenza emotiva' : 'Daniel Goleman',
-'Madame Bovary' : 'Gustave Flaubert',
-'Jane Eyre' : 'Charlotte Brontë',
-'Little Women' : 'Louisa May Alcott',
-'Anna Karenina' : 'Leo Tolstoy',
-'The Alchemist' : 'Paulo Coelho',
-'The Da Vinci Code' : 'Dan Brown',
-'The Three Musketeers' : 'Alexandre Dumas',
-'Les Miserables' : 'Victor Hugo',
-'War and Peace' : 'Leo Tolstoy',
-'Treasure Island' : ' Robert Louis Stevenson',
-'David Copperfield' : 'Charles Dickens',
-'Charlie And The Chocolate Factory' : 'Roald Dahl',
-'Jane Eyre' : 'Charlotte Brontë',
-'Crime And Punishment' : 'Fyodor Dostoyevsky',
-'Memoirs Of A Geisha' : 'Arthur Golden',
-'Matilda' : 'Roald Dahl',
-'Dracula' : 'Bram Stoker',
-'The Green Mile' : 'Stephen King',
-'It' : 'Stephen King',
-'Thief Of Time' : 'Terry Pratchett',
-'Moby Dick' : 'Herman Melville',
-'Oliver Twist' : 'Charles Dickens',
-'The Witches' : 'Roald Dahl',
-'Frankenstein' : 'Mary Shelley',
-'American Psycho' : 'Bret Easton Ellis',
-<<<<<<< HEAD
-)
-
-
-=======
-'People We Meet on Vacation': 'Emily Henry',
-'Malibu Rising' : 'Taylor Jenkins Reid',
-'The Four Winds' : 'Kristin Hannah',
-'The Last Thing He Told Me' : 'Laura Dave',
-'The Lincoln Highway' : 'Amor Towles',
-'Project Hail Mary' : 'Andy Weir',
-'Klara and the Sun' : 'Kazuo Ishiguro',
-'The Push' : 'Ashley Audrain',
-'Beautiful World, Where Are You' : 'Sally Rooney',
-'Apples Never Fall' : 'Laine Moriarty',
-'Under the Whispering Door' : 'T.J. Klune',
-'Cloud Cuckoo Land' : 'Anthony Doerr',
-'Crying in H Mart: A Memoir' : 'Michelle Zauner',
-'Think Again: The Power of Knowing What You Dont Know' : 'Adam Grant',
-'Between Two Kingdoms: A Memoir of a Life Interrupted' : 'Suleika Jaquad',
-'The Anthropocene Reviewed' : 'John Green',
-'How To Avoid A Climate Disaster: The Solutions We Have and the Breakthroughs We Need' : 'Bill Gates',
-'What Happened To You?: Conversations on Trauma, Resilience, and Healing' : 'Oprah Winfrey',
-'Empire of Pain: The Secret History of the Sackler Dynasty' : 'Patrick Radden Keefe,
-'The Sum of Us: What Racism Costs Everyone and How We Can Prosper Together' : 'Heather McGhee',
-}
->>>>>>> 82d7f56682c4018841e88a23541bf83f8b757c1d
-
-
+list_of_books = {
+'Romeo And Juliet': ['William Shakespeare', 1964, 'Tragedy', 322],
+ '1984': ['George Orwell', 1949, 'Science Fiction', 408 ],
+ '2001: A Space Odissey': ['Arthur C. Clarke', 1968, 'Science Fiction', 256],
+ 'Pride And Prejudice' : ['Jane Austen', 1813, 'Romance', 329],
+ 'The Great Gatsby' : ['F.Scott Fitzgerald', 1925, 'Tragedy', 240],
+ 'The Lord Of The Rings' : ['J. R. R. Tolkien', 1937, 'Fantasy', 9250],
+ 'The Old Man And The Sea' : ['Ernest Hemingway', 1951, 'Fiction', 127],
+ 'The Picture Of Dorian Gray' : ['Oscar Wilde', 1890, 'Philosophy', 232],
+ 'A Christmas Carol' : ['Charles Dickens',1843, 'Drama', 112],
+ 'I Promessi Sposi' : ['Alessandro Manzoni', 1827, 'Romance', 571],
+ 'Awaken The Giant Within' : ['Anthony Robbins', 1995, 'Self growth', 544],
+ 'La Divina Commedia' : ['Dante Allighieri', 1909, 'Epic poem', 656],
+ 'Odyssey' : ['Omero', 1937, 'Epic poem', 414],
+ 'Lo Scudo Di Talos' : ['Valerio Massimo Manfredi', 1988, 'Romance', 318],
+ 'Delitto E Castigo' : ['Dostoevskij', 1967, 'Tragedy', 596],
+ 'I Giorni Dell Eternita' : ['Ken Follet', 2019, 'Fition', 1220],
+ 'Fu Sera Fu Mattina' : ['Ken Follet', 2021, 'Fiction', 792],
+ 'La Caduta Dei Giganti' : ['Ken Follet', 2017, 'Romance', 999],
+ 'La Cruna Dell Ago' : ['Ken Follet', 2002, 'Thriller',  380],
+ 'Mondo Senza Fine' : ['Ken Follet', 2000, 'Fiction', 1200],
+ 'L Arte Della Guerra' : ['Sun Tzu', 1948, 'Treaty', 96],
+ 'Alla Ricerca Del Tempo Perduto' : ['Marcel Proust', 1967, 'Romance', 3724],
+ 'La Coscienza Di Zeno' : ['Italo Svevo', 1965, 'Romance', 345],
+ 'Il Processo' : ['Franz Kafka', 1924, 'Romance', 256],
+ 'Uno, Nessuno, Centomila' : ['Luigi Pirandello', 1929, 'Romance', 224],
+ #'Gita Al Faro' : ['Virginia Wolf', 1980, 'Romance',], #pagine non trovate
+ 'Il Processo' : ['Franz Kafka', 1989, 'Romance', 245],
+ 'La Fabbrica Di Cioccolato' : ['Roald Dahl', 1988, 'Adventure', 202],
+ 'Billy Elliot' : ['Melvin Burgess', 1999, 'Musical', 224],
+ 'Il Nome Della Rosa' : ['Umberto Eco', 1979, 'Romance', 624],
+ 'Intelligenza Emotiva' : ['Daniel Goleman', 2001, 'Self growth', 486],
+ # 'Cosa Vuoi Davvero?' : ['Roberto Re', 2004, 'Empowerment'] #pagine non torovate
+ 'Madame Bovary' : ['Gustave Flaubert', 1968, 'Romance', 386],
+ 'Jane Eyre' : ['Charlotte Bronte', 1974, 'Fiction', 596],
+ 'Little Women' : ['Louisa May Alcott', 1954, 'Romance', 432],
+ 'Anna Karenina' : ['Leo Tolstoy', 1968, 'Romance', 1107],
+ 'The Alchemist' : ['Paulo Coelho', 2001, 'Romance', 217],
+ 'The Da Vinci Code' : ['Dan Brown', 2004, 'Romance', 524],
+ 'The Three Musketeers' : ['Alexandre Dumas', 1922, 'Romance', 721],
+ 'Les Miserables' : ['Victor Hugo', 1933, 'Romance', 947],
+ 'War And Peace' : ['Leo Tolstoy', 1931, 'Romance', 1415 ],
+ 'Treasure Island' : ['Robert Louis Stevenson', 1967, 'Romance', 287],
+ 'David Copperfield' : ['Charles Dickens', 1975, 'Romance', 768],
+ 'Jane Eyre' : ['Charlotte Bronte', 1996, 'Romance', 560],
+ 'Memoirs Of A Geisha' : ['Arthur Golden', 1966, 'Memoire', 512],
+ 'Matilda' : ['Roald Dahl', 1977, 'Humor',224],
+ 'Dracula' : ['Bram Stoker', 1966, 'Thriller', 624],
+ 'The Green Mile' : ['Stephen King', 1979, 'Documentary', 448],
+ 'It' : ['Stephen King', 1986, 'Science fiction', 1315],
+ 'Thief Of Time' : ['Terry Pratchett', 1965, 'Fantasy', 384],
+ 'Moby Dick' : ['Herman Melville', 1851, 'Epic', 798],
+ 'Oliver Twist' : ['Charles Dickens', 1923, 'Adventure', 538],
+ 'The Witches' : ['Roald Dahl', 1945, 'Commedy', 336],
+ 'Frankenstein' : ['Mary Shelley', 1890, 'Science fiction',440],
+ 'American Psycho' : ['Bret Easton Ellis', 1967, 'Horror', 522],
+ 'People We Meet On Vacation': ['Emily Henry', 1959, 'Romance', 384],
+ 'Malibu Rising' : ['Taylor Jenkins Reid', 2004, 'Fiction', 384],
+ 'The Four Winds' : ['Kristin Hannah', 2009, 'Thriller', 464],
+ 'The Last Thing He Told Me' : ['Laura Dave', 2017, 'Thriller', 320],
+ 'The Lincoln Highway' : ['Amor Towles', 1970, 'Fiction', 600],
+ 'Project Hail Mary' : ['Andy Weir', 1957, 'Romance', 496],
+ 'Klara And The Sun' : ['Kazuo Ishiguro', 1995, 'Romance',307],
+ 'The Push' : ['Ashley Audrain', 1992, 'Thriller', 320],
+ 'Beautiful World, Where Are You' : ['Sally Rooney', 1986, 'Romance', 374],
+ 'Apples Never Fall' : ['Laine Moriarty', 1984, 'Thriller', 480],
+ 'Under The Whispering Door' : ['T.J. Klune', 1998, 'Romance', 384],
+ 'Cloud Cuckoo Land' : ['Anthony Doerr', 1957, 'Science fiction', 656],
+ 'Crying In H Mart: A Memoir' : ['Michelle Zauner', 1924, 'Momoir', 256],
+ 'Think Again: The Power Of Knowing What You Dont Know' : ['Adam Grant', 1990, 'Self growth', 320],
+ 'Between Two Kingdoms: A Memoir Of A Life Interrupted' : ['Suleika Jaquad', 2007, 'Memoir', 368],
+ 'The Anthropocene Reviewed' : ['John Green', 2004, 'Commentary', 304],
+ 'How To Avoid A Climate Disaster: The Solutions We Have And The Breakthroughs We Need' : ['Bill Gates', 2018, 'Essay', 272],
+# 'What Happened To You?: Conversations On Trauma, Resilience, And Healing' : ['Oprah Winfrey', 2012, 'Self growth'],
+ 'Empire Of Pain: The Secret History Of The Sackler Dynasty' : ['Patrick Radden Keefe', 2003, 'Crime', 560],
+ 'The Sum Of Us: What Racism Costs Everyone And How We Can Prosper Together' : ['Heather McGhee', 2007, 'Commedy', 448],
+ }
 
 def check_book(title):
     if title in list_of_books:
-        print("The book {} is written by {}".format(title, list_of_books[title]))
+        field = list_of_books[title]
+        author = field[0]
+        year = field[1]
+        genre = field[2]
+        pages = field[3]
+        
+        print("The title {} is written by {}, and it was published in {}".format(title, author, year, pages))
+        #print ("The title {} is a {} book written by {}, published in year {} and counts {} words".format(title, author, genre, year, pages,))
+        
     else:
-        print("Sorry, we do not have {}".format(title))
+        print("The title {} is not present in our catalogue".format(title))
 
 def check_author(author_name):
-
     found = False
-    for title, author in list_of_books.items():
+    for title, field in list_of_books.items():
+        author = field[0]
+        year = field[1]
+        genre = field[2]
+        pages = field[3]
         if author == author_name:
-            print("{} wrote {}".format(author_name, title))
+            print("{} published {} in {} ".format(author_name, title, year, pages))
             found = True
 
     if not found:
-        print("Sorry, {} is not present.".format(author_name))
+        print("Sorry we don't have any book by {} ".format(author_name))
+
+def check_year(year):
+    found = False
+    for title, field in list_of_books.items():
+        author = field[0]
+        pub_year = field[1]
+        genre = field[2]
+        pages = field[3]
+        if year == pub_year:
+            print("{} published {} in {} ".format(author, title, year, pages))
+            found = True
+
+    if not found:
+        print("Sorry we don't have any books published in the year {} ".format(year))
+
+
+def check_genre(book_genre):
+     found = False
+     for title, field in list_of_books.items():
+         author = field[0]
+         pub_year = field[1]
+         genre = field[2]
+         pages = field[3]
+         if genre == book_genre:
+             print("{} of {} is a {} ".format(title, author, genre, pages))
+             found = True
+
+     if not found:
+            print("Sorry there aren't books in the {} category, try again".format(genre))
+            
+def check_pages(book_pages):
+     found = False
+     for title, field in list_of_books.items():
+         author = field[0]
+         pub_year = field[1]
+         genre = field[2]
+         pages = field[3]
+         if pages == book_pages:
+             print("{} of {} is a {} and it has {} pages".format(title, author, genre, pages))
+             found = True
+
+     if not found:
+            print("Sorry there aren't books in the {} category, try again".format(genre))
+            
